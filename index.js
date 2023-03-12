8fcac6422e029d0b40d1816b923d25acce812a9c
cheng <y2984491@gmail.com>
2023-03-12 03:07:18 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+$(".hamburger, #navbar a").on("click", () => {
+	let currentVisibility = $("#navbar").css("visibility");
+	let newVisibility = currentVisibility === "hidden" ? "visible" : "hidden";
+
+	if (newVisibility == "hidden") {
+		$("#navbar").addClass("visibilty");
+	} else {
+		$("#navbar").removeClass("visibilty");
+	}
+})
